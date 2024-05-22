68017f88dc4cf283e89826fab499082e3d151d7d
Martin <146810557+mrtnhwttktc@users.noreply.github.com>
2024-05-22 17:03:50 +0900

    Improvement in the logger package Implemented verbose and debug flag for all commands chore: Remove unused code and comments in config.go, language.go, root.go, and logger.go

--- cmd/language.go
+++ cmd/language.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -15,7 +12,7 @@ import (
 
 // languageCmd represents the language command
 var languageCmd = &cobra.Command{
-	Use:   "language",
+	Use:   "language [english|japanese]",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -23,7 +20,15 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	Example: `
+	# interactive mode
+	ktcli config language
+
+	# args mode
+	ktcli config language english
+	`,
 	Run: func(cmd *cobra.Command, args []string) {
+
 		langs := localizer.GetLangOptions()
 
 		prompt := promptui.Select{
@@ -33,27 +38,17 @@ to quickly create a Cobra application.`,
 
 		_, result, err := prompt.Run()
 		if err != nil {
-			slog.Error("Prompt failed: %v\n", err)
+			slog.Error("Language selection prompt failed", slog.String("error", err.Error()))
 			return
 		}
 		fmt.Println(localizer.Lang.Translate("You selected: %s", result))
 		err = config.SaveToConfig("language", result)
 		if err != nil {
-			slog.Error("Error saving language to config: %v\n", err)
+			slog.Error("Error saving language to config.", slog.String("error", err.Error()))
 		}
 	},
 }
 
 func init() {
 	configCmd.AddCommand(languageCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// languageCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// languageCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
