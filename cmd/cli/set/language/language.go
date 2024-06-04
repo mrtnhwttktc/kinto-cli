d839aadbb19d67f15b407b1bd046adfced127688
Martin <146810557+mrtnhwttktc@users.noreply.github.com>
2024-06-04 10:38:30 +0900

    improvement in logger and config added convenience flags updated translations added version check and update reminder

--- cmd/cli/set/language/language.go
+++ cmd/cli/set/language/language.go
@@ -2,6 +2,7 @@ package language
 
 import (
 	"fmt"
+	"io"
 	"log/slog"
 	"os"
 
@@ -25,55 +26,70 @@ func NewLanguageCmd() *cobra.Command {
 	ktcli set language english
 	`,
 		PreRun: func(cmd *cobra.Command, args []string) {
+			out := cmd.OutOrStdout()
 			n, _ := cmd.Flags().GetBool("non-interactive")
 			if n && len(args) == 0 {
-				slog.Error("Cannot use non-interactive mode with set language command and no arguments. A language must be provided as an argument.")
-				fmt.Println(l.Translate("Cannot use non-interactive mode with set language command and no arguments. A language must be provided as an argument.\n"))
+				fmt.Fprintln(out, l.Translate("Cannot use non-interactive mode with set language command and no arguments. A language must be provided as an argument.\n"))
 				cmd.Help()
 				os.Exit(1)
 			}
 			if len(args) > 1 {
-				slog.Error("Too many arguments provided for set language command.")
-				fmt.Println(l.Translate("Too many arguments provided for set language command. Please select a language from the following: %v\n", localizer.GetLangOptions()))
+				fmt.Fprintln(out, l.Translate("Too many arguments provided for set language command. Please select a language from the following: %v\n", localizer.GetLangOptions()))
 				cmd.Help()
 				os.Exit(1)
 			}
 		},
 		Run: func(cmd *cobra.Command, args []string) {
+			out := cmd.OutOrStdout()
 			langs := localizer.GetLangOptions()
-			conf := config.GetConfig()
 			if len(args) == 1 {
-				fmt.Println(l.Translate("You selected: %s", args[0]))
-				err := config.SaveToConfig(conf, "language", args[0])
-				if err != nil {
-					slog.Error("Error saving language to config.", slog.String("error", err.Error()))
-					fmt.Println(l.Translate("Error saving language to config."))
+				for _, lang := range langs {
+					if lang == args[0] {
+						setLanguage(out, l, args[0])
+						return
+					}
+				}
+				fmt.Fprintln(out, l.Translate("Invalid language %s provided. Valid languages: %v\n", args[0], langs))
+				n, _ := cmd.Flags().GetBool("non-interactive")
+				if n {
 					os.Exit(1)
 				}
-				return
+				fmt.Fprintln(out, l.Translate("Please select a language."))
 			}
 
-			prompt := promptui.Select{
-				Label: l.Translate("Select the language to use"),
-				Items: langs,
-			}
-
-			_, result, err := prompt.Run()
+			result, err := interactiveMode(l, langs)
 			if err != nil {
-				slog.Error("Language selection prompt failed", slog.String("error", err.Error()))
 				return
 			}
-			fmt.Println(l.Translate("You selected: %s", result))
-			err = config.SaveToConfig(conf, "language", result)
-			if err != nil {
-				slog.Error("Error saving language to config.", slog.String("error", err.Error()))
-				fmt.Println(l.Translate("Error saving language to config."))
-				os.Exit(1)
-			}
+			setLanguage(out, l, result)
 		},
 	}
 	setFlags(languageCmd, l)
 	return languageCmd
+}
+
+func interactiveMode(l *localizer.Localizer, langs []string) (string, error) {
+	prompt := promptui.Select{
+		Label: l.Translate("Select the language to use"),
+		Items: langs,
+	}
+
+	_, result, err := prompt.Run()
+	if err != nil {
+		slog.Error("Language selection prompt failed", slog.String("error", err.Error()))
+		return "", err
+	}
+	return result, nil
+}
+
+func setLanguage(out io.Writer, l *localizer.Localizer, lang string) {
+	conf := config.GetConfig()
+	fmt.Fprintln(out, l.Translate("You selected: %s", lang))
+	err := config.SaveToConfig(conf, "language", lang)
+	if err != nil {
+		fmt.Fprintln(out, l.Translate("Error saving language to config."))
+		os.Exit(1)
+	}
 }
 
 func setFlags(cmd *cobra.Command, l *localizer.Localizer) {
--- internal/version/version.go
+++ internal/version/version.go
@@ -1,5 +1,34 @@
 package version
 
+import (
+	"log/slog"
+	"net/http"
+)
+
 // Version is the current version of the CLI.
 // Updated by the build process
 var Version = "dev"
+var binURL = ""
+
+// Check if a new version is available.
+func IsNewVersionAvailable() bool {
+	resp, err := http.Head(binURL)
+	if err != nil {
+		slog.Warn("Error checking for new version. Head request returned an error.", slog.String("error", err.Error()))
+		return false
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		slog.Warn("Error checking for new version. Received a non 200 response code.", slog.String("status", resp.Status))
+		return false
+	}
+	metadataVersin := resp.Header.Get("x-aws-version")
+	if metadataVersin == "" {
+		slog.Warn("Error checking for new version. No version header found.")
+		return false
+	}
+	if metadataVersin != Version && Version != "dev" {
+		return true
+	}
+	return false
+}
